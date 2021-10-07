d0859bba6bcb3a5abcf3b381a8afdfb111b64779
peter <peter@jbaron.com>
2021-10-07 12:45:38 +0200

    updated colab build script

--- bin/build_colab.sh
+++ bin/build_colab.sh
@@ -4,4 +4,5 @@ apt update -q  &> /dev/null
 echo "Install Java..."
 apt-get install -q openjdk-11-jdk-headless &> /dev/null
 echo "Install Jupyter Kotlin kernel..."
-conda install -c jetbrains kotlin-jupyter-kernel
+pip install kotlin-jupyter-kernel
+
