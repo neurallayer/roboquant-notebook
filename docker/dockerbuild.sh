f349035299b975481819594862eaf7995516d62d
Peter Dekkers <peter@jbaron.com>
2021-11-11 21:23:53 +0100

    Cross architecture build

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -1,5 +1,13 @@
 [[ ! -f "LICENSE" ]] && echo "run the dockerbuild script from the project root directory like this: ./docker/dockerbuild.sh" && exit -1
 
-docker build \
--t roboquant/jupyter:latest \
--f docker/Dockerfile .
+# Traditional docker build
+# docker build -t roboquant/jupyter:latest -f docker/Dockerfile .
+
+# New Docker builder that supports building for multi-arch
+# Run the following lines once to switch to new builder
+# docker buildx create --name mybuilder
+# docker buildx use mybuilder
+# docker buildx inspect --bootstrap
+
+# Build for Intel and Apple Silicon
+docker buildx build --platform linux/amd64,linux/arm64 -t roboquant/jupyter:latest -f docker/Dockerfile --push .
