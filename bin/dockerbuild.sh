748d6b9584b0223d5a4136f7297a7dfa3d71980c
Peter Dekkers <peter@jbaron.com>
2022-08-26 16:33:01 +0200

    updated sample notebook

--- bin/dockerbuild.sh
+++ bin/dockerbuild.sh
@@ -1,4 +1,4 @@
-[[ ! -f "LICENSE" ]] && echo "run the dockerbuild.sh script from the project root directory like this: ./bin/dockerbuild.sh" && exit 1
+[[ ! -f "LICENSE" ]] && echo "run the script from the project root directory like this: ./bin/dockerbuild.sh" && exit 1
 
 # New Docker builder that supports building for multi-architectures
 # Run the following 3 lines once to enable the new builder
@@ -7,9 +7,8 @@
 # docker buildx inspect --bootstrap
 
 # Clean output cells of notebooks
-cd notebooks
-jupyter nbconvert --clear-output --inplace *.ipynb
-cd ..
+rm -f notebooks/*.nbconvert.ipynb
+jupyter nbconvert --clear-output --inplace notebooks/*.ipynb
 
 # Build for Intel and Apple Silicon
 docker buildx build --platform linux/amd64,linux/arm64 -t roboquant/jupyter:latest --push .
--- bin/dockerbuild_singleplatform.sh
+++ bin/dockerbuild_singleplatform.sh
@@ -1,10 +1,8 @@
 [[ ! -f "LICENSE" ]] && echo "run the  script from the project root directory like this: ./bin/dockerbuild_singleplatform.sh" && exit 1
 
-
 # Clean output cells of notebooks
-cd notebooks
-jupyter nbconvert --clear-output --inplace *.ipynb
-cd ..
+rm -f notebooks/*.nbconvert.ipynb
+jupyter nbconvert --clear-output --inplace notebooks/*.ipynb
 
 # Build for local platform only
 docker build -t roboquant/jupyter:latest .
