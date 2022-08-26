4d022187654ffa3c8fa4ea4ce2d64bcac637cce0
Peter Dekkers <peter@jbaron.com>
2022-08-26 23:00:00 +0200

    fixed two notebooks

--- bin/run_notebooks.sh
+++ bin/run_notebooks.sh
@@ -3,8 +3,13 @@
 [[ ! -f "LICENSE" ]] && echo "run the script from the project root directory like this: ./bin/run_notebooks.sh" && exit 1
 
 rm -f notebooks/*.nbconvert.ipynb
+rm notebooks/error.log
 
 for notebook in backtest charts policies strategies tech-analysis
 do
-  jupyter nbconvert --to notebook --execute "./notebooks/$notebook.ipynb"
+  jupyter nbconvert --to notebook --execute "./notebooks/$notebook.ipynb" 2>> notebooks/error.log
 done
+
+rm -f notebooks/*.nbconvert.ipynb
+grep -i exception notebooks/error.log && echo "Found exceptions" && exit 1
+rm notebooks/error.log
