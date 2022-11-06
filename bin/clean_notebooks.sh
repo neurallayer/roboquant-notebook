280a589a8ec3c241588c4dfc41896672ad3d510f
Peter Dekkers <peter@jbaron.com>
2022-11-06 20:52:11 +0100

    Updated notebooks and fix mistakes from previous versions

--- bin/clean_notebooks.sh
+++ bin/clean_notebooks.sh
@@ -2,5 +2,7 @@
 
 [[ ! -f "LICENSE" ]] && echo "run the script from the project root directory like this: ./bin/clean_notebooks.sh" && exit 1
 
+# Clear the output cells in all the notebooks. This should be done before they are
+# being checked-in git
 jupyter nbconvert --clear-output --inplace ./notebooks/*.ipynb
 
